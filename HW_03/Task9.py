ab144e0adf8a8645e28c44d54a2bf2949f9c1af9
alinakonysheva <konysheva.alina@gmail.com>
2019-01-04 18:47:37 +0100

    поправила комментарии в 4 и 9

--- HW_03/Task9.py
+++ HW_03/Task9.py
@@ -7,25 +7,33 @@ SIZE1 = 3
 SIZE2 = 3
 MIN_ITEM = 0
 MAX_ITEM = 100
-matrix = [[random.randint(MIN_ITEM, MAX_ITEM) for i in range(SIZE1)] for j in range(SIZE2)]
+matrix1 = [[random.randint(MIN_ITEM, MAX_ITEM) for i in range(SIZE1)] for j in range(SIZE2)]
+
 # выводим матрицу
-print("Матрица")
+print("_____Матрица____")
 print('________________')
-for line in matrix:
+for line in matrix1:
     for i, j in enumerate(line):
         print(f'{j:>5}', end='')
     print('  ')
+print('________________')
+
 print("Минимальные элементы в каждом столбце матрицы:")
 print('________________')
+# список, в который будем складывать минимальные значенения в каждом столбце
 set_min = []
-for i in range(SIZE2):
-    min_in_column = matrix[0][i]
-    for j in range(SIZE1):
-        if matrix[j][i] < min_in_column:
-            min_in_column = matrix[j][i]
+
+# перебираем столбцы
+for i in range(SIZE1):
+    min_in_column = matrix1[0][i]
+    # построчно перебираем элементы в столбце и ищем минимальный
+    for j in range(SIZE2):
+        if matrix1[j][i] < min_in_column:
+            min_in_column = matrix1[j][i]
+    # формируем список из минимальных элементов в каждом столбце
     set_min.append(min_in_column)
     print(f'{min_in_column:>5}', end='')
-
+# ищем максимальный элемент в списке минимальных элементов
 max_in_line = set_min[0]
 for i in set_min:
     if i > max_in_line:
