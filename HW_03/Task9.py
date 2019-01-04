6034eb7fdbcdd8e85fc768649be8306193fdcf1d
alinakonysheva <konysheva.alina@gmail.com>
2019-01-04 18:04:08 +0100

    девятая работает, добавила красивенький вывод

--- HW_03/Task9.py
+++ HW_03/Task9.py
@@ -1,14 +1,6 @@
 # 9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
 
-# матрица с максимальным элементом среди минимальных -- 7
-# matrix_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-# def search_max_from_min(matrix):
-
-# max_j_in_i = 0
-
-# print(matrix_1[0][1])
-
+# создаем матрицу
 import random
 
 SIZE1 = 3
@@ -16,12 +8,15 @@ SIZE2 = 3
 MIN_ITEM = 0
 MAX_ITEM = 100
 matrix = [[random.randint(MIN_ITEM, MAX_ITEM) for i in range(SIZE1)] for j in range(SIZE2)]
-
-print("Матрица и минимальный элемент в каждом столбце:")
+# выводим матрицу
+print("Матрица")
+print('________________')
 for line in matrix:
     for i, j in enumerate(line):
         print(f'{j:>5}', end='')
     print('  ')
+print("Минимальные элементы в каждом столбце матрицы:")
+print('________________')
 set_min = []
 for i in range(SIZE2):
     min_in_column = matrix[0][i]
@@ -36,5 +31,4 @@ for i in set_min:
     if i > max_in_line:
         max_in_line = i
 print('\n')
-print(f'Максимальный элемент среди минимальных элементов столбцов матрицы = {max_in_line}')
-
+print(f'Максимальный элемент среди минимальных элементов столбцов матрицы: {max_in_line}')
