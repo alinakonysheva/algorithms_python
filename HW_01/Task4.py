1a12f607e5bf51141b95adae6f6fcc1dae01d95b
alinakonysheva <konysheva.alina@gmail.com>
2019-01-08 19:46:59 +0100

    добавила в 4 вывод с переменным и ф-строками для читаемости, добавила try/except в генерацию символа

--- HW_01/Task4.py
+++ HW_01/Task4.py
@@ -8,34 +8,38 @@
 
 import random
 
+output_words_low = 'нижнюю границу диапазона в котором надо генерировать  --> '
+output_words_high = 'верхнюю границу диапазона в котором надо генерировать  --> '
 a = 1
 while a != 0:
     try:
-        print('Выберете, пожалуйста, элемент какого типа нужно сгенерировать.')
+        print('Какого типа элемент нужно сгенерировать?')
         generation_type = int(input('1 -- целое число, 2 -- вещественное число, 3 -- символ? -->  '))
 
         if generation_type == 1:
             start_generation = int(
-                input('Задайте целое число, нижнюю границу диапазона в котором надо генерировать  -->  '))
-            end_generation = int(input('Задайте целое число, верхнюю границу диапазона -->  ')) + 1
+                input(f'Задайте целое число, {output_words_low}'))
+            end_generation = int(input(f'Задайте целое число, {output_words_high}'))
             print('Ваше случайное целое число:')
             print(random.randint(start_generation, end_generation))
             a = int(input('Если хотите дальше генерировать, нажмите что-нибудь, 0 -- для выхода  '))
         elif generation_type == 2:
             start_generation = float(
-                input('Задайте число, нижнюю границу диапазона в котором надо генерировать  -->  '))
-            end_generation = float(input('Задайте число, верхнюю границу диапазона -->  ')) + 1
+                input(f'Задайте число, {output_words_low}'))
+            end_generation = float(input(f'Задайте число, {output_words_high}'))
             print('Ваше случайное вещественное число:')
             print(random.uniform(start_generation, end_generation))
             a = int(input('Если хотите дальше генерировать, нажмите что-нибудь, 0 -- для выхода  '))
         elif generation_type == 3:
-            start_generation = int(
-                ord(str(input('Задайте символ, нижнюю границу диапазона в котором надо генерировать  -->  '))))
-            end_generation = int(ord(str(input('Задайте символ, верхнюю границу диапазона -->  ')))) + 1
-            print('Ваше случайный символ:')
-            print(chr(random.randint(start_generation, end_generation)))
-            a = int(input('Если хотите дальше генерировать, нажмите что-нибудь, 0 -- для выхода  '))
-
+            try:
+                start_generation = int(
+                    ord(str(input(f'Задайте символ, {output_words_low}'))))
+                end_generation = int(ord(str(input(f'Задайте символ, {output_words_high}'))))
+                print('Ваше случайный символ:')
+                print(chr(random.randint(start_generation, end_generation)))
+                a = int(input('Если хотите дальше генерировать, нажмите что-нибудь, 0 -- для выхода  '))
+            except TypeError:
+                print('Если хотите получить сгенерированный символ, вводите буквы, символы или цифры (не числа)')
         else:
             print('У нас тут только выбор из трех целых чисел имеется: 1, 2 и 3:)')
     except ValueError:
