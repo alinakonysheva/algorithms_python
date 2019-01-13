d310d0a6f65e1e698ee8c2f96c4bbab4e9fa98a5
alinakonysheva <konysheva.alina@gmail.com>
2019-01-13 21:10:19 +0100

    1, разные варианты решения обернула в функции

--- HW_04/Task1.py
+++ HW_04/Task1.py
@@ -14,7 +14,7 @@ array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
 
-def search_max_negative(list_init):
+def search_max_negative_1(list_init):
     list_negative = []
     for i in list_init:
         if i < 0:
@@ -27,30 +27,43 @@ def search_max_negative(list_init):
     return - min_item, list_init.index(- min_item)
 
 
-eggs = search_max_negative(array)
-print(f" 1-й метод. Максимальный отрицательный элемент в массиве: {eggs[0]}")
-print(f" 1-й метод. Место максимального отрицательного элемента в массиве: {eggs[1]}")
+# eggs = search_max_negative_1(array)
+# print(f" 1-й метод. Максимальный отрицательный элемент в массиве: {eggs[0]}")
+# print(f" 1-й метод. Место максимального отрицательного элемента в массиве: {eggs[1]}")
+
 
 # 2-е решение:
-def search_max_negative(list_init):
+def search_max_negative_2(list_init):
+    max_negative = float("-inf")
 
-    return
-max_negative = float("-inf")
+    for i in list_init:
+        if i < 0:
+            if i > max_negative:
+                max_negative = i
 
-for i in array:
-    if i < 0:
-        if i > max_negative:
-            max_negative = i
-print(f" 2-й метод. Максимальный отрицательный элемент в массиве: {max_negative}")
-print(" 2-й метод. Место максимального отрицательного элемента в массиве: ", end='')
-print(f"{list(i for i, e in enumerate(array) if e == max_negative)[0]}")
+    index_max_negative = list(i for i, e in enumerate(list_init) if e == max_negative)[0]
+
+    return max_negative, index_max_negative
+
+
+# spam = search_max_negative_2(array)
+# print(f" 2-й метод. Максимальный отрицательный элемент в массиве: {spam[0]}")
+# print(f" 2-й метод. Место максимального отрицательного элемента в массиве: {spam[1]}")
+
+
 
 # 3-e решение:
-array_negative = []
-for i in array:
-    if i < 0:
-        array_negative.append(i)
-max_negative = max(array_negative)
-print(f" 3-й метод. Максимальный отрицательный элемент в массиве: {max_negative}")
-print(f"{list(i for i, e in enumerate(array) if e == max_negative)[0]}")
+def search_max_negative_3(list_init):
+    array_negative = []
+    for i in list_init:
+        if i < 0:
+            array_negative.append(i)
+    max_negative = max(array_negative)
+    index_max_negative = list(i for i, e in enumerate(list_init) if e == max_negative)[0]
 
+    return max_negative, index_max_negative
+
+
+# eggs = search_max_negative_3(array)
+# print(f" 3-й метод. Максимальный отрицательный элемент в массиве: {eggs[0]}")
+# print(f" 3-й метод. Место максимального отрицательного элемента в массиве: {eggs[1]}")
