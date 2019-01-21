029d505ce018cb0ca5ae4b2014ec511f6688c9d6
alinakonysheva <konysheva.alina@gmail.com>
2019-01-21 15:21:57 +0100

    1 done

---
+++ HW_05/Task1.py
@@ -0,0 +1,66 @@
+# 1. Пользователь вводит данные о количестве предприятий, их наименования и прибыль за 4 квартала
+# (т.е. 4 отдельных числа) для каждого предприятия.. Программа должна определить среднюю прибыль
+# (за год для всех предприятий) и вывести наименования предприятий,
+# чья прибыль выше среднего и отдельно вывести наименования предприятий, чья прибыль ниже среднего.
+import collections
+import statistics
+
+print('Посчитаем среднюю прибыль для предпрятий и выведем компании, у которых она больше или меньше средней.')
+count_companies = int(input('Введите, пожалуйста, количество предприятий:   '))
+Company = collections.namedtuple('Company', ['name', 'quarter1', 'quarter2', 'quarter3', 'quarter4', 'profit'])
+database = []
+profits = []
+for company in range(count_companies):
+    profile = list(input(
+        'Через точку с запятой (;): название компании; прибыль за первый кваратал; второй; третий; четвёртый--> ').split(
+        ';'))
+    profit = (float(profile[1]) + float(profile[2]) + float(profile[3]) + float(profile[4]))
+    company = Company(name=profile[0], quarter1=profile[1], quarter2=profile[2], quarter3=profile[3],
+                      quarter4=profile[4], profit=profit)
+
+    database.append(company)
+    profits.append(company.profit)
+
+avg = statistics.mean(profits)
+succeeded = []
+failed = []
+average = []
+for company in database:
+    if company.profit > avg:
+        succeeded.append(company.name)
+    elif company.profit < avg:
+        failed.append(company.name)
+    else:
+        average.append(company.name)
+
+print(f'Средняя годовая прибыль по компаниям: {avg}')
+
+if len(succeeded) > 0:
+    print('Выше среднегодовой по компаниям: ')
+    for company in succeeded:
+        print(f'{company}')
+if len(failed) > 0:
+    print('Ниже срежнегодовой по компаниям:')
+    for company in failed:
+        print(f'{company}')
+if len(succeeded) == 0 and len(failed) == 0 and len(average) > 0:
+    print(f'Среднегодовая прибыль одинаковая у всех компаний:')
+    for company in average:
+        print(f'{company}')
+
+########################
+# Тестовые данные
+########################
+# taart; 10; 9; 8; 7
+# maart; 11; 19; 18; 17
+# baart; 21; 29; 28; 27
+########################
+# taart; 5; 15; 1; 19
+# maart; 11; 9; 12; 8
+# baart; 0; 0; 0; 40
+########################
+# taart; 0; 1; 1; 0
+# maart; 0; 0; 1; 0
+# baart; 0; 1; 0; 0
+# waart; 0; 1; 1; 1
+# kaart; 1; 0; 1; 1
