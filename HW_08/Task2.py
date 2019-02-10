83a6b69b11dcdf962d957d868c93f43f85e5d359
alinakonysheva <konysheva.alina@gmail.com>
2019-02-10 22:22:34 +0100

    первая и вторая работает

--- HW_08/Task2.py
+++ HW_08/Task2.py
@@ -4,12 +4,14 @@ import collections
 import operator
 
 decoded_string = 'Liberte, Egalite, Fraternite'
+print(f'Исходная строка: {decoded_string}')
 value_weight = collections.Counter(decoded_string)
 sorted_value_weight = []
+# преобразуем строку в словарь из кортежей символ-вес
 sorted_value_weight = sorted(value_weight.items(), key=operator.itemgetter(1))
 
 
-# создание узла дерева:
+# созданем класс узла дерева:
 
 class MyNode:
     def __init__(self, value, freq, left=None, right=None):
@@ -37,44 +39,51 @@ class MyNode:
         return f'MyNode(freq = {self.freq}, value = {self.value}, left = {self.left}, right = {self.right})'
 
 
-nods = collections.deque([])
+# преобразуем все элементы словаря в узлы:
+nodes = collections.deque([])
 for i in range(len(sorted_value_weight)):
-    nods.append(MyNode(sorted_value_weight[i][0], sorted_value_weight[i][1]))
+    nodes.append(MyNode(sorted_value_weight[i][0], sorted_value_weight[i][1]))
 
 
+# сворачиваем два узла в один:
 def pack(node_1, node_2):
     left = node_1 if node_1.get_freq() <= node_2.get_freq() else node_2
     right = node_2 if node_1.get_freq() <= node_2.get_freq() else node_1
     return MyNode(None, freq=node_1.get_freq() + node_2.get_freq(), left=left, right=right)
 
 
-def reorder(nodes):
-    return collections.deque(sorted(nodes, key=lambda x: x.get_freq()))
+# сортируем узлы по возрастанию частоты вхождения:
+def reorder(nods):
+    return collections.deque(sorted(nods, key=lambda x: x.get_freq()))
 
 
-while len(nods) > 1:
-    node1 = nods.popleft()
-    node2 = nods.popleft()
+# сворачиваем список узлов в дерево:
+while len(nodes) > 1:
+    node1 = nodes.popleft()
+    node2 = nodes.popleft()
     node = pack(node1, node2)
-    nods.appendleft(node)
-    nods = reorder(nods)
+    nodes.appendleft(node)
+    nodes = reorder(nodes)
 
 
-def go_through_tree(node, code, elemnts_codes):
-    if node is None:
+# проходим по дереву узлов, рекурсивно собираем '0', если прошли слева, и '1', если прошли справа
+def go_through_tree(my_node, path, elements_codes):
+    # для пустого узла возвращаем пустой список
+    if my_node is None:
         return []
-    elif node.is_leaf():
-        elemnts_codes.append((node.get_value(), code))
-        return elemnts_codes
+    # если это лист, то добавляем "код символа"
+    elif my_node.is_leaf():
+        elements_codes.append((my_node.get_value(), path))
+        return elements_codes
+    # рассчитываем коды для левой и правой ветвей узла
     else:
-        ltree = go_through_tree(node.get_left(), code + '0', [])
-        rtree = go_through_tree(node.get_right(), code + '1', [])
-        return elemnts_codes + ltree + rtree
+        codes_left = go_through_tree(my_node.get_left(), path + '0', [])
+        codes_right = go_through_tree(my_node.get_right(), path + '1', [])
+        return elements_codes + codes_left + codes_right
 
 
-lst = go_through_tree(nods.pop(), '', [])
 encoded_string = []
-encode_dictionary = dict(lst)
+encode_dictionary = dict(go_through_tree(nodes.pop(), '', []))
 print('Закодированная строка: ', end='')
 for i in decoded_string:
     encoded_string.append(encode_dictionary[i])
